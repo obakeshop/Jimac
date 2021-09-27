4154bf924a4272db012c6ae5b8748ff1d667a3c5
OkuriSae <sae.okuri@gmail.com>
2021-09-27 15:13:28 +0900

    fix update index

--- script.js
+++ script.js
@@ -8,46 +8,69 @@ const vue = Vue.createApp({
       textColor: "#3333FF",
       fontFamily: "'Kiwi Maru', serif",
       fontSize: "42pt",
-      textAnim: true
+      textAnim: false
     }
   },
   methods: {
     changeLylics(e) {
+      console.log(1);
       localStorage.setItem('myCat', this.lylics);
       this.items = (this.lylics || "").split('\n').map(e => { return { lylic: e }; });
       this.updateLylic();
     },
-    setIndex(e) {
-      const value = parseInt(e.target.value);
-      if (!value || value < 0 || this.items.length < value) {
+    setIndex(value) {
+      if (this.index === value) {
+        return;
+      }
+
+      if (!value || value < 0 || this.items.length-1 < value) {
         this.index = 0;
       } else {
         this.index = value;
       }
-    },
-    moveIndex(i) {
-      const newIdx = this.index + i;
-      this.index = (0 <= newIdx && newIdx <= this.items.length-1) ? newIdx : 0;
       this.updateLylic();
     },
     updateLylic() {
-      this.currentLylic = ""; 
+      if (this.textAnim) {
+        this.currentLylic = ""; // Reset animation
+      }
       setTimeout(() => {
         this.currentLylic = this.items[this.index].lylic;
         $("#lylic-test-after").addClass("lylic-style after");
         $("#lylic-test-main").addClass("lylic-style main");
         $("#lylic-test-before").addClass("lylic-style before");
-      }, 50);
+      }, 50); // Animation waiting
     },
     keyEvent(event) {
       const tagName = document.activeElement.tagName; 
-      if (tagName === "TEXTAREA" || tagName === "INPUT") {
+      if (tagName === "INPUT") {
+        return;
+
+      } else if (tagName === "TEXTAREA") {
+        switch (event.keyCode) {
+          case 13: 
+          case 37: 
+          case 38: 
+          case 39: 
+          case 40: 
+            this.updateIndex();
+            break;
+        }
+
+      } else {
+        switch (event.keyCode) {
+          case 37: this.moveIndex(-1); break;
+          case 39: this.moveIndex(+1); break;
+        }
+      }
+    },
+    updateIndex() {
+      if (document.activeElement.tagName !== "TEXTAREA") {
         return;
       }
-      switch (event.keyCode) {
-        case 37: this.moveIndex(-1); break;
-        case 39: this.moveIndex(+1); break;
-      }
+      const match = this.lylics.substr(0, document.activeElement.selectionStart).match(/\n/g);
+      const index = match ? match.length : 0;
+      this.setIndex(index);
     }
   },
   filters: {
@@ -62,8 +85,8 @@ const vue = Vue.createApp({
       this.changeLylics();
     })
   },
-}).mount('#jimakuGenerator')
+}).mount('#jimakuGenerator');
 
-$(window).keydown(function(event){
+$(window).keyup(function(event){
   vue.keyEvent(event);
 });
