af97fe865610726b72d5e9d53896c5256601f114
OkuriSae <sae.okuri@gmail.com>
2021-09-28 03:41:43 +0900

    add anim variations

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const jimakuGenerator = Vue.createApp({
       jimakuTextColor: "#3333FF", // 字幕のテキストカラー
       jimakuFontFamily: "'Kiwi Maru', serif", // 字幕フォントファミリー
       jimakuFontSize: "42pt", // 字幕フォントサイズ
-      jimakuAnim: false, // 字幕をアニメーションさせるか
+      jimakuAnim: "none", // 字幕をアニメーションさせるか
       jimakuOutline: true, // 字幕の縁取り
     }
   },
