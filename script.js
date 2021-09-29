8f909543d21c9529ad84f1fa8bbf4e8876972223
OkuriSae <sae.okuri@gmail.com>
2021-09-29 18:17:37 +0900

    some updates

--- script.js
+++ script.js
@@ -19,45 +19,75 @@ const jimakuGenerator = Vue.createApp({
       jimakuOutline: true, // 字幕の縁取り
       preview: false, // プレビューモード
       filter: '', // レパートリー検索条件
+      movieType: '', // 動画プラットフォーム
       movieUrl: '', // 動画URL
+      movieSrc: '', // 動画埋め込みURL
+      tags: '', // 検索用タグ
+      db: { // Access to localStorage
+        getSongs() {
+          return JSON.parse(localStorage.getItem("songs"));
+        },
+        getSelectedId() {
+          return localStorage.getItem("selectedId");
+        },
+        getSong(id) {
+          return JSON.parse(localStorage.getItem(id));
+        },
+        setSongs(songs) {
+          localStorage.setItem("songs", JSON.stringify(songs));
+        },
+        setSelectedId(id) {
+          localStorage.setItem("selectedId", id);
+        },
+        setSong(songId, data) {
+          localStorage.setItem(songId, JSON.stringify({ 
+            lyric: data.lyric, 
+            jimakuFontSize: data.jimakuFontSize, 
+            movieUrl: data.movieUrl, 
+            tags: data.tags, 
+          }));
+        },
+      },
     }
   },
 
   methods: {
 
-    loadRepertory() { // レパートリーを最新状態にする
-      this.repertory = JSON.parse(localStorage.getItem("songs")).map( id => {
-        return { id, title: localStorage.getItem(id).split('\n')[0], tags: localStorage.getItem(id) }; 
-      });
-    },
-    
+    /** Utilities **/
+    getlyrics() { return (this.lyric || '').split('\n'); },
+
+    /** Read Methods **/
     loadSong(songId) { // データをローカルストレージから読込み
+      const data = this.db.getSong(songId);
       this.target = songId;
-      this.lyric = localStorage.getItem(songId);
+      this.lyric = data.lyric;
+      this.jimakuFontSize = data.jimakuFontSize;
+      this.movieUrl = data.movieUrl;
+      this.tags = data.tags;
       $('#lyric').prop('selectionStart', 0); 
       this.changelyric();
     },
-    
-    saveSong(songId, lyric) {
-      this.target = songId;
-      this.lyric = lyric;
-      localStorage.setItem(this.target, this.lyric);
-    },
-    
-    selectSong(id) { // 指定した曲を表示し、選択状態にする
-      this.loadSong(id);
-      this.setJimakuIndex(0);
-      localStorage.setItem("selectedId", id);
+
+    loadRepertory() { // レパートリーを最新状態にする
+      this.repertory = this.db.getSongs().map( id => {
+        return { 
+          id, 
+          title: this.db.getSong(id).lyric.split('\n')[0], 
+          tags: this.db.getSong(id).tags,
+        }; 
+      });
     },
 
+    /** Data modifying Methods **/
     createSong() { // データ新規作成
       this.target = Date.now();
       this.lyric = lyricTemplate;
-      localStorage.setItem(this.target, this.lyric);
+      this.jimakuFontSize = '42pt';
+      this.movieUrl = '';
+      this.tags = '';
+      this.db.setSong(this.target, this.$data);
 
-      let songs = JSON.parse(localStorage.getItem("songs")) || [];
-      songs.push(this.target);
-      localStorage.setItem("songs", JSON.stringify(songs));
+      this.db.setSongs([this.target].concat(this.db.getSongs() || []));
 
       this.loadRepertory();
       this.selectSong(this.target);
@@ -65,13 +95,46 @@ const jimakuGenerator = Vue.createApp({
     },
 
     changelyric() { // 歌詞が更新されたときに保存して画面更新
-      localStorage.setItem(this.target, this.lyric);
+      this.db.setSong(this.target, this.$data);
+      this.update();
+    },
+
+    deleteSong() { // データ消去
+
+      if (this.lyric !== lyricTemplate && !confirm('本当に消去しますか？')) {
+        return;
+      }
+
+      // 削除対象のデータをフィルタして上書き保存
+      let songs = this.db.getSongs().filter( id => {
+        return id !== this.target;
+      });
+      this.db.setSongs(songs);
+      localStorage.removeItem(this.target);
+
+      if (!songs.length) { // 全部消しちゃったら１個新規で作る
+        songs.push(this.createSong());
+      }
+      
+      this.selectSong(songs[0]);
+      this.update();
+    },
+
+    /** Controll Methods **/
+
+    update() {
       this.updateJimaku();
+      this.updateMovieUrl();
       this.loadRepertory();
     },
 
-    getlyrics() { return (this.lyric || '').split('\n'); },
-
+    selectSong(id) { // 指定した曲を表示し、選択状態にする
+      this.loadSong(id);
+      this.setJimakuIndex(0);
+      this.db.setSelectedId(id);
+      this.update();
+    },
+    
     updateJimaku() { // 字幕の更新
       if (this.jimakuAnim) {
         this.jimaku = ""; // Reset animation
@@ -84,7 +147,7 @@ const jimakuGenerator = Vue.createApp({
         $("#jimaku-2").addClass("jimaku-2");
       }, 50); // Animation waiting
     },
-
+    
     setJimakuIndex(value) { // 字幕行番号の変更
       if (this.jimakuIndex !== value) {
         const invalid = v => !v || v < 0 || this.getlyrics().length-1 < v;
@@ -103,47 +166,47 @@ const jimakuGenerator = Vue.createApp({
         this.updateJimakuIndex();
       }
     },
+    
+    updateMovieUrl() { // 動画情報更新
+      this.db.setSong(this.target, this.$data);
 
-    deleteSong() { // データ消去
-
-      if (this.lyric !== lyricTemplate && !confirm('本当に消去しますか？')) {
-        return;
-      }
-
-      // 削除対象のデータをフィルタして上書き保存
-      let songs = JSON.parse(localStorage.getItem("songs")).filter( id => {
-        return id !== this.target;
-      });
-      localStorage.setItem("songs", JSON.stringify(songs));
-      localStorage.removeItem(this.target);
-
-      if (!songs.length) { // 全部消しちゃったら１個新規で作る
-        songs.push(this.createSong());
-      }
-      
-      this.loadRepertory();
-      this.selectSong(songs[0]);
-      this.updateJimaku();
-    },
-
-    movieUrlRefresh() {
       const backup = this.movieUrl;
       const nicoElement = document.getElementById('nico');
+      this.movieType = "";
       this.movieUrl = "";
+      this.movieSrc = "";
       nicoElement.innerHTML = "";
+
       setTimeout(() => {
         this.movieUrl = backup;
-        // niconico は scriptタグでvueコンポーネントにできないので手動追加
-        if (this.movieUrl.indexOf('nico') !== -1) {
+
+        if (this.movieUrl.indexOf('youtu') !== -1) {
+          const match = /[/?=]([-\w]{11})/.exec(this.movieUrl);
+          this.movieSrc = `https://www.youtube.com/embed/${ match ? match[1] : '' }`
+          this.movieType = 'yt';
+
+        } else if (this.movieUrl.indexOf('nico') !== -1) {
+          this.movieType = 'nico';
+          // niconico は scriptタグでvueコンポーネントにできないので手動追加
           const nicoId = this.movieUrl.match(/(?:sm|nm|so|ca|ax|yo|nl|ig|na|cw|z[a-e]|om|sk|yk)\d{1,14}\b/)[0];
           const script = document.createElement('script');
+          this.movieSrc = `https://embed.nicovideo.jp/watch/${nicoId}/script?w=408&h=230`;
           script.setAttribute('type', 'application/javascript');
-          script.setAttribute('src', `https://embed.nicovideo.jp/watch/${nicoId}/script?w=408&h=230`);
+          script.setAttribute('src', this.movieSrc);
           nicoElement.appendChild(script);
         }
       }, 50); // Refresh delay
     },
     
+    updateJimakuFontSize() {
+      this.db.setSong(this.target, this.$data);
+    },
+    
+    updateTags() {
+      this.db.setSong(this.target, this.$data);
+      this.update();
+    },
+
     windowKeyEvent(event) { // ショートカットキー制御
       const tagName = document.activeElement.tagName; 
       if (["INPUT", "TEXTAREA"].includes(tagName)) {
@@ -157,13 +220,14 @@ const jimakuGenerator = Vue.createApp({
     },
   },
 
-  mounted: function () { // ウィンドウ読込み時の初期化
+  /** Initialize **/
+  mounted: function () {
     this.$nextTick(function() {
       if (!localStorage.length) {
         this.createSong();
       } else {
         this.loadRepertory();
-        this.selectSong(localStorage.getItem("selectedId"));
+        this.selectSong(this.db.getSelectedId());
       }
     })
   },
@@ -175,4 +239,4 @@ $(window).keyup(function(event) {
   jimakuGenerator.windowKeyEvent(event);
 });
 
-new Clipboard('#jimaku');
+new Clipboard('#jimaku'); // clipboard.js
